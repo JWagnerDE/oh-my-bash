6d025ea1efdb7b07d9678186d03a45583fbc0e1a
Jannis Wagner <jannis.wagner@microdrones.com>
2023-06-19 16:27:11 +0200

    Added fonts as submodule and neovim aliases file

---
+++ aliases/nvim.aliases.sh
@@ -0,0 +1,8 @@
+#! bash oh-my-bash.module
+#  ---------------------------------------------------------------------------
+
+# NeoVim Aliases
+alias v='nvim'
+alias vi='nvim'
+alias vim='nvim'
+alias n='nvim'
